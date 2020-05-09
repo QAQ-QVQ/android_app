9408c0adb8b7fdcb7a5a80b38290ee88071cd66c
zhangqingyou <zhangqingyou@zhangqingyou.com>
2020-05-09 18:29:04 +0800

    飒沓

--- app/src/main/java/com/hjy/gamecommunity/App.java
+++ app/src/main/java/com/hjy/gamecommunity/App.java
@@ -49,7 +49,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        initModule();/////
+        initModule();//大苏打撒
         seetingJPush();//极光推送
         jAnalyticsInterface();//极光页面统计
         jshare();//极光分享
