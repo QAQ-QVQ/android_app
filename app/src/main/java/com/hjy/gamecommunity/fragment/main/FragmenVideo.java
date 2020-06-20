aeb4fed69e26840419716e2b60d83b6558277644
张风 <1334738402@qq.com>
2020-06-20 17:27:40 +0800

    1.自定义tab 1. 可自定义下划线   2.可根据字体大小自适应高度

--- app/src/main/java/com/hjy/gamecommunity/fragment/main/FragmenVideo.java
+++ app/src/main/java/com/hjy/gamecommunity/fragment/main/FragmenVideo.java
@@ -3,6 +3,7 @@ package com.hjy.gamecommunity.fragment.main;
 import android.view.View;
 
 import com.hjy.baseui.ui.BaseFragment;
+import com.hjy.baseui.ui.view.tablayout.TabLayoutX;
 import com.hjy.gamecommunity.R;
 
 /**
@@ -12,14 +13,17 @@ import com.hjy.gamecommunity.R;
  * Des:
  */
 public class FragmenVideo extends BaseFragment {
+    private TabLayoutX mTabLayout;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_video;
+
     }
 
     @Override
     public void initView(View mRootView) {
-
+        mTabLayout = findViewById(R.id.tabLayout);
     }
 
     @Override
@@ -31,11 +35,29 @@ public class FragmenVideo extends BaseFragment {
 
     @Override
     public void initData() {
+        mTabLayout.addTab(mTabLayout.newTab().setText("直播"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("游戏视频"));
+         mTabLayout.setTabHeight(-1);
 
     }
 
     @Override
     public void listener() {
+        mTabLayout.addOnTabSelectedListener(new TabLayoutX.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayoutX.Tab tab) {
 
+            }
+
+            @Override
+            public void onTabUnselected(TabLayoutX.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayoutX.Tab tab) {
+
+            }
+        });
     }
 }
